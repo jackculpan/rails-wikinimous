47198c3d6fafc1be143f8e6913077221e77f62ff
jack culpan <jackculpan@me.com>
2019-07-30 14:13:38 +0800

    added functionality, needs styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # get 'articles', to: 'articles#index', as: :articles_index
+  # get 'articles/show/:id', to: 'articles#show', as: :articles_show
+
+  # get 'articles/new', to: 'articles#add', as: :articles_add
+  # post 'articles', to: 'articles#create', as: :articles_create
+
+  # get "articles/:id/edit", to: "articles#edit", as: :articles_edit
+  # patch "articles/:id", to: "articles#update", as: :articles_update
+
+  # delete "articles/:id", to: "articles#destroy", as: :articles_destroy
+
+  resources :articles
 end
